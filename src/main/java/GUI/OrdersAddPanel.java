f5828e3a832ed8aab1b724695a1cdddfb333923c
ElinaNadav <elina.nadav@gmail.com>
2019-05-16 22:03:51 +0300

    Added Order panels

---
+++ src/main/java/GUI/OrdersAddPanel.java
@@ -0,0 +1,271 @@
+package main.java.GUI;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+
+public class OrdersAddPanel extends IWorkPanel{
+    private JLabel providerLabel;
+    private JLabel categoryLabel;
+    private JLabel itemNameLabel;
+    private JLabel unitsLabel;
+    private JLabel orderSumLabel;
+    private JLabel orderSumFieldLabel;
+    private JComboBox providersList;
+    private JComboBox categoryList;
+    private JComboBox itemList;
+    private JTextField unitsTF;
+    private JButton searchItemButton;
+    private JButton placeOrderButton;
+    private JTable itemsTable;
+    private JTable orderTable;
+    private JScrollPane scrollItemsTable;
+    private JScrollPane scrollOrderTable;
+    private JPanel searchPanel;
+    private JPanel tablesPanel;
+    private JPanel itemsTablePanel;
+    private JPanel orderTablePanel;
+    private JPanel placeOrderPanel;
+    private JSplitPane splitTables;
+
+    private String[] itemsColumnNames = {"SKU","Item name","Category","Provider","Available units","Cost per Item","Expected delivery","Action"};
+    private String[] orderColumnNames = {"SKU","Item name","Category","Provider","Units","Cost per Item","Expected delivery","Action"};
+
+    private String[][] testData ={{"555","Shubi","kabubi","shabubi","2","20.8.15","2","20.8.15"},{"123","Halo","this is dog","kuku","5","555","2","20.8.15"}};
+
+    public OrdersAddPanel(){
+        initialization();
+        setSearchPanelLayout();
+        setTableLayout();
+        setPlaceOrderLayout();
+        setMainLayout();
+    }
+
+    @Override
+    protected void initialization() {
+        providerLabel = new JLabel("Provider: ");
+        categoryLabel = new JLabel("Category: ");
+        itemNameLabel = new JLabel("Item name: ");
+        unitsLabel = new JLabel("Units: ");
+        orderSumLabel = new JLabel("Order Sum: ");
+        orderSumFieldLabel = new JLabel("0");
+        providersList = new JComboBox();
+        categoryList = new JComboBox();
+        itemList = new JComboBox();
+        unitsTF = new JTextField(10);
+        searchItemButton = new JButton("Search Item");
+        placeOrderButton = new JButton("Place Order");
+        itemsTable = new JTable(testData,itemsColumnNames);
+        orderTable = new JTable(testData,orderColumnNames);
+        scrollItemsTable = new JScrollPane(itemsTable,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollOrderTable = new JScrollPane(orderTable,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        searchPanel = new JPanel();
+        itemsTablePanel = new JPanel();
+        orderTablePanel = new JPanel();
+        placeOrderPanel = new JPanel();
+        tablesPanel = new JPanel();
+        //splitTables = new JSplitPane(JSplitPane.VERTICAL_SPLIT,itemsTablePanel,orderTablePanel);
+    }
+
+    @Override
+    protected void setMainLayout() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gcMainPanel = new GridBagConstraints();
+        gcMainPanel.fill = GridBagConstraints.HORIZONTAL;
+
+        gcMainPanel.ipady = 50;
+        gcMainPanel.gridy = 0;
+        gcMainPanel.weightx = 0.5;
+        gcMainPanel.weighty = 0.1;
+
+        gcMainPanel.gridx = 0;
+        gcMainPanel.anchor = GridBagConstraints.NORTH;
+        add(searchPanel, gcMainPanel);
+
+        gcMainPanel.ipady = 0;
+        gcMainPanel.gridy ++;
+        gcMainPanel.weightx = 0.5;
+        gcMainPanel.weighty = 0.1;
+
+        gcMainPanel.gridheight = 2;
+        gcMainPanel.gridx = 0;
+        gcMainPanel.anchor = GridBagConstraints.CENTER;
+        add(tablesPanel, gcMainPanel);
+
+        gcMainPanel.ipady = 0;
+        gcMainPanel.gridy ++;
+        gcMainPanel.weightx = 0.5;
+        gcMainPanel.weighty = 0.1;
+
+        gcMainPanel.gridheight = 1;
+        gcMainPanel.gridx = 0;
+        gcMainPanel.anchor = GridBagConstraints.SOUTH;
+        add(placeOrderPanel, gcMainPanel);
+    }
+
+    @Override
+    protected void setTableLayout() {
+        setItemsTable();
+        setOrderTable();
+
+        tablesPanel.setLayout(new GridBagLayout());
+        tablesPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        //tablesPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        GridBagConstraints gcTablesPanel = new GridBagConstraints();
+        gcTablesPanel.fill = GridBagConstraints.BOTH;
+
+        gcTablesPanel.gridx = 0;
+        gcTablesPanel.gridy = 0;
+        gcTablesPanel.weightx = 1;
+        gcTablesPanel.weighty = 1;
+        tablesPanel.add(itemsTablePanel,gcTablesPanel);
+
+        gcTablesPanel.gridx = 0;
+        gcTablesPanel.gridy = 1;
+        gcTablesPanel.weightx = 1;
+        gcTablesPanel.weighty = 1;
+        tablesPanel.add(orderTablePanel,gcTablesPanel);
+    }
+
+    protected void setItemsTable(){
+        itemsTablePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        itemsTablePanel.setLayout(new GridBagLayout());
+        itemsTablePanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        GridBagConstraints gcTablePanel = new GridBagConstraints();
+        gcTablePanel.fill = GridBagConstraints.HORIZONTAL;
+
+        gcTablePanel.gridx = 0;
+        gcTablePanel.gridy = 0;
+        gcTablePanel.weightx = 1;
+        gcTablePanel.weighty = 0.1;
+        //gcTablePanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        itemsTablePanel.add(itemsTable,gcTablePanel);
+    }
+    protected void setOrderTable(){
+        orderTablePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        orderTablePanel.setLayout(new GridBagLayout());
+        orderTablePanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        GridBagConstraints gcTablePanel = new GridBagConstraints();
+        gcTablePanel.fill = GridBagConstraints.HORIZONTAL;
+
+        gcTablePanel.gridx = 0;
+        gcTablePanel.gridy = 0;
+        gcTablePanel.weightx = 1;
+        gcTablePanel.weighty = 0.1;
+
+        orderTablePanel.add(orderTable,gcTablePanel);
+    }
+
+    @Override
+    protected void setSearchPanelLayout() {
+        /////// Set combo-box ///////
+        DefaultComboBoxModel providersModel = new DefaultComboBoxModel();
+        providersModel.addElement("provider 1");
+        providersList.setModel(providersModel);
+        DefaultComboBoxModel categoryModel = new DefaultComboBoxModel();
+        categoryModel.addElement("category 1");
+        categoryList.setModel(categoryModel);
+
+        searchPanel.setBorder(BorderFactory.createTitledBorder("Orders"));
+        searchPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gcSearchPanel = new GridBagConstraints();
+        gcSearchPanel.fill = GridBagConstraints.CENTER;
+        gcSearchPanel.insets = new Insets(5,5,5,5);
+
+        /////// First row ///////
+        gcSearchPanel.gridy = 0;
+        gcSearchPanel.weightx = 1;
+        gcSearchPanel.weighty = 0.1;
+        gcSearchPanel.gridx = 0;
+
+        /////// Next row //////
+        gcSearchPanel.gridy ++;
+        gcSearchPanel.weightx = 0.5;
+        gcSearchPanel.weighty = 0.1;
+        gcSearchPanel.gridx = 0;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(providerLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 1;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(providersList, gcSearchPanel);
+
+        gcSearchPanel.gridx = 2;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(categoryLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 3;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(categoryList, gcSearchPanel);
+
+        /////// Next row ///////
+        gcSearchPanel.gridy ++;
+
+        gcSearchPanel.weightx = 0.5;
+        gcSearchPanel.weighty = 0.1;
+
+        gcSearchPanel.gridx = 0;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(itemNameLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 1;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(itemList, gcSearchPanel);
+
+        gcSearchPanel.gridx = 2;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(unitsLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 3;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(unitsTF, gcSearchPanel);
+
+        gcSearchPanel.gridx = 4;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(searchItemButton, gcSearchPanel);
+
+        ///// align fields sizes //////
+        Dimension fieldSize = unitsTF.getPreferredSize();
+        providersList.setPreferredSize(fieldSize);
+        categoryList.setPreferredSize(fieldSize);
+        itemList.setPreferredSize(fieldSize);
+        searchItemButton.setPreferredSize(fieldSize);
+    }
+
+    protected void setPlaceOrderLayout(){
+        placeOrderPanel.setLayout(new GridBagLayout());
+        placeOrderPanel.setBorder(BorderFactory.createSoftBevelBorder(BevelBorder.RAISED));
+        GridBagConstraints gcPlaceOrderPanel = new GridBagConstraints();
+        gcPlaceOrderPanel.fill = GridBagConstraints.CENTER;
+        gcPlaceOrderPanel.insets = new Insets(5,5,5,5);
+
+        /////// First row ///////
+        gcPlaceOrderPanel.gridy = 0;
+        gcPlaceOrderPanel.weightx = 1;
+        gcPlaceOrderPanel.weighty = 0.1;
+        gcPlaceOrderPanel.gridx = 0;
+
+        /////// Next row //////
+        gcPlaceOrderPanel.gridy ++;
+        gcPlaceOrderPanel.weightx = 0.5;
+        gcPlaceOrderPanel.weighty = 0.1;
+        gcPlaceOrderPanel.gridx = 0;
+        gcPlaceOrderPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        placeOrderPanel.add(orderSumLabel, gcPlaceOrderPanel);
+
+        gcPlaceOrderPanel.gridx = 1;
+        gcPlaceOrderPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        placeOrderPanel.add(orderSumFieldLabel, gcPlaceOrderPanel);
+
+        gcPlaceOrderPanel.gridx = 2;
+        gcPlaceOrderPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        placeOrderPanel.add(placeOrderButton, gcPlaceOrderPanel);
+
+        ///// align fields sizes //////
+        Dimension fieldSize = unitsTF.getPreferredSize();
+        orderSumFieldLabel.setPreferredSize(fieldSize);
+        placeOrderButton.setPreferredSize(fieldSize);
+    }
+}
--- src/main/java/GUI/OrdersSearchPanel.java
+++ src/main/java/GUI/OrdersSearchPanel.java
@@ -3,36 +3,164 @@ package main.java.GUI;
 import javax.swing.*;
 import java.awt.*;
 
-public class OrdersSearchPanel extends JPanel {
-    private JButton searchItemButton;
-    private JButton placeOrder;
+public class OrdersSearchPanel extends IWorkPanel {
+    private JLabel providerLabel;
+    private JLabel categoryLabel;
+    private JLabel itemNameLabel;
+    private JLabel orderIdLabel;
+    private JComboBox providersList;
+    private JComboBox categoryList;
+    private JComboBox itemList;
+    private JTextField orderIdTF;
+    private JButton searchOrderButton;
+    private JTable ordersTable;
+    private JScrollPane scrollTable;
+    private JPanel searchPanel;
+    private JPanel tablePanel;
+
+    private String[] columnNames = {"Order ID","Items quantity","Cost","Date received"};
+    private String[][] testData ={{"555","5","999","10.05.19"},{"123","6","502","15.05.19"}};
+
     public OrdersSearchPanel(){
+            initialization();
+            setSearchPanelLayout();
+            setTableLayout();
+            setMainLayout();
+    }
 
-        searchItemButton = new JButton("Search item");
-        placeOrder = new JButton("Place order ");
+    @Override
+    protected void initialization(){
+        providerLabel = new JLabel("Provider: ");
+        categoryLabel = new JLabel("Category: ");
+        itemNameLabel = new JLabel("Item name: ");
+        orderIdLabel = new JLabel("Order ID: ");
+        providersList = new JComboBox();
+        categoryList = new JComboBox();
+        itemList = new JComboBox();
+        orderIdTF = new JTextField(10);
+        searchOrderButton = new JButton("Search order");
+        ordersTable = new JTable(testData,columnNames);
+        scrollTable = new JScrollPane(ordersTable,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        searchPanel = new JPanel();
+        tablePanel = new JPanel();
+    }
 
+    @Override
+    protected void setMainLayout() {
         setLayout(new GridBagLayout());
-        GridBagConstraints gc = new GridBagConstraints();
-        gc.fill = GridBagConstraints.NONE;
+        GridBagConstraints gcMainPanel = new GridBagConstraints();
+        gcMainPanel.fill = GridBagConstraints.HORIZONTAL;
 
+        gcMainPanel.ipady = 50;
+        gcMainPanel.gridy = 0;
+        gcMainPanel.weightx = 0.5;
+        gcMainPanel.weighty = 0.1;
+
+        gcMainPanel.gridx = 0;
+        gcMainPanel.anchor = GridBagConstraints.PAGE_START;
+        add(searchPanel, gcMainPanel);
+
+        gcMainPanel.ipady = 0;
+        gcMainPanel.gridy ++;
+        gcMainPanel.weightx = 0.5;
+        gcMainPanel.weighty = 0.1;
+
+        gcMainPanel.gridx = 0;
+        gcMainPanel.anchor = GridBagConstraints.CENTER;
+        add(tablePanel, gcMainPanel);
+    }
+
+    @Override
+    protected void setTableLayout() {
+        tablePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        tablePanel.setLayout(new GridBagLayout());
+        tablePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        GridBagConstraints gcTablePanel = new GridBagConstraints();
+        gcTablePanel.fill = GridBagConstraints.HORIZONTAL;
+
+        gcTablePanel.gridx = 0;
+        gcTablePanel.gridy = 0;
+        gcTablePanel.weightx = 1;
+        gcTablePanel.weighty = 0.1;
+
+        tablePanel.add(scrollTable,gcTablePanel);
+    }
+
+    @Override
+    protected void setSearchPanelLayout() {
+        /////// Set combo-box ///////
+        DefaultComboBoxModel providersModel = new DefaultComboBoxModel();
+        providersModel.addElement("provider 1");
+        providersList.setModel(providersModel);
+        DefaultComboBoxModel categoryModel = new DefaultComboBoxModel();
+        categoryModel.addElement("category 1");
+        categoryList.setModel(categoryModel);
+
+        searchPanel.setBorder(BorderFactory.createTitledBorder("Orders"));
+        searchPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gcSearchPanel = new GridBagConstraints();
+        gcSearchPanel.fill = GridBagConstraints.CENTER;
+        gcSearchPanel.insets = new Insets(5,5,5,5);
 
         /////// First row ///////
-        gc.weightx = 1;
-        gc.weighty = 0.5;
+        gcSearchPanel.gridy = 0;
+        gcSearchPanel.weightx = 1;
+        gcSearchPanel.weighty = 0.1;
+        gcSearchPanel.gridx = 0;
 
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.anchor = GridBagConstraints.CENTER;
-        add(searchItemButton, gc);
+        /////// Next row //////
+        gcSearchPanel.gridy ++;
+        gcSearchPanel.weightx = 0.5;
+        gcSearchPanel.weighty = 0.1;
+        gcSearchPanel.gridx = 0;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(providerLabel, gcSearchPanel);
 
-        /////// Second row ///////
-        gc.weightx = 1;
-        gc.weighty = 0.5;
+        gcSearchPanel.gridx = 1;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(providersList, gcSearchPanel);
 
-        gc.gridx = 0;
-        gc.gridy = 1;
-        gc.anchor = GridBagConstraints.CENTER;
-        add(placeOrder, gc);
+        gcSearchPanel.gridx = 2;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(categoryLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 3;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(categoryList, gcSearchPanel);
+
+        /////// Next row ///////
+        gcSearchPanel.gridy ++;
+
+        gcSearchPanel.weightx = 0.5;
+        gcSearchPanel.weighty = 0.1;
+
+        gcSearchPanel.gridx = 0;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(itemNameLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 1;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(itemList, gcSearchPanel);
+
+        gcSearchPanel.gridx = 2;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+        searchPanel.add(orderIdLabel, gcSearchPanel);
+
+        gcSearchPanel.gridx = 3;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(orderIdTF, gcSearchPanel);
+
+        gcSearchPanel.gridx = 4;
+        gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
+        searchPanel.add(searchOrderButton, gcSearchPanel);
+
+        ///// align fields sizes //////
+        Dimension fieldSize = orderIdTF.getPreferredSize();
+        providersList.setPreferredSize(fieldSize);
+        categoryList.setPreferredSize(fieldSize);
+        itemList.setPreferredSize(fieldSize);
+        searchOrderButton.setPreferredSize(fieldSize);
     }
 
 }
