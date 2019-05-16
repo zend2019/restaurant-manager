cd183bcb027c88da2bdb60d72de228e0d3858277
ElinaNadav <elina.nadav@gmail.com>
2019-05-16 19:17:21 +0300

    Enhanced Inventory panel

--- src/main/java/GUI/InventoryPanel.java
+++ src/main/java/GUI/InventoryPanel.java
@@ -17,22 +17,41 @@ public class InventoryPanel extends JPanel {
     private JButton addButton;
     private JTable inventoryTable;
     private JScrollPane scrollTable;
-    private JPanel searchPanel = new JPanel();
-    private JPanel tablePanel = new JPanel();
+    private JPanel searchPanel;
+    private JPanel tablePanel;
 
     private String[] columnNames = {"SKU","Item name","Category","Provider","Quantity","Date received"};
     private String[][] testData ={{"555","Shubi","kabubi","shabubi","2","20.8.15"},{"123","Halo","this is dog","kuku","5","555"}};
 
     public InventoryPanel(){
         initialization();
+        setSearchPanelLayout();
+        setTableLayout();
+        setMainLayout();
+    }
 
-        /////// Set main layout ///////
+    private void initialization(){
+        //inventoryPage = new JLabel("Inventory");
+        providerLabel = new JLabel("Provider: ");
+        categoryLabel = new JLabel("Category: ");
+        itemNameLabel = new JLabel("Item Name: ");
+        quantityLabel = new JLabel("Quantity: ");
+        providersList = new JComboBox();
+        categoryList = new JComboBox();
+        itemNameTF = new JTextField(10);
+        quantityTF = new JTextField(10);
+        searchButton = new JButton("Search");
+        addButton = new JButton("Add");
+        inventoryTable = new JTable(testData,columnNames);
+        scrollTable = new JScrollPane(inventoryTable,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        searchPanel = new JPanel();
+        tablePanel = new JPanel();
+    }
+
+    private void setMainLayout(){
         setLayout(new GridBagLayout());
         GridBagConstraints gcMainPanel = new GridBagConstraints();
         gcMainPanel.fill = GridBagConstraints.HORIZONTAL;
-
-        setSearchPanelLayout();
-        setTableLayout();
 
         gcMainPanel.ipady = 50;
         gcMainPanel.gridy = 0;
@@ -51,30 +70,13 @@ public class InventoryPanel extends JPanel {
         gcMainPanel.gridx = 0;
         gcMainPanel.anchor = GridBagConstraints.CENTER;
         add(tablePanel, gcMainPanel);
-
-
-    }
-
-    private void initialization(){
-        //inventoryPage = new JLabel("Inventory");
-        providerLabel = new JLabel("Provider: ");
-        categoryLabel = new JLabel("Category: ");
-        itemNameLabel = new JLabel("Item Name: ");
-        quantityLabel = new JLabel("Quantity: ");
-        providersList = new JComboBox();
-        categoryList = new JComboBox();
-        itemNameTF = new JTextField(10);
-        quantityTF = new JTextField(10);
-        searchButton = new JButton("Search");
-        addButton = new JButton("Add");
-        inventoryTable = new JTable(testData,columnNames);
-        scrollTable = new JScrollPane(inventoryTable,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     }
 
     private void setTableLayout(){
         tablePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         tablePanel.setLayout(new GridBagLayout());
+        tablePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         GridBagConstraints gcTablePanel = new GridBagConstraints();
         gcTablePanel.fill = GridBagConstraints.HORIZONTAL;
 
@@ -99,9 +101,9 @@ public class InventoryPanel extends JPanel {
         searchPanel.setLayout(new GridBagLayout());
         GridBagConstraints gcSearchPanel = new GridBagConstraints();
         gcSearchPanel.fill = GridBagConstraints.CENTER;
+        gcSearchPanel.insets = new Insets(5,5,5,5);
 
         /////// First row ///////
-        gcSearchPanel.insets = new Insets(5,5,5,5);
         gcSearchPanel.gridy = 0;
         gcSearchPanel.weightx = 1;
         gcSearchPanel.weighty = 0.1;
@@ -160,5 +162,15 @@ public class InventoryPanel extends JPanel {
         gcSearchPanel.gridx = 4;
         gcSearchPanel.anchor = GridBagConstraints.FIRST_LINE_START;
         searchPanel.add(searchButton, gcSearchPanel);
+
+        ///// align button sizes //////
+        Dimension buttonSize = searchButton.getPreferredSize();
+        addButton.setPreferredSize(buttonSize);
+
+        ///// align combo box sizes //////
+        Dimension comboSize = itemNameTF.getPreferredSize();
+        providersList.setPreferredSize(comboSize);
+        categoryList.setPreferredSize(comboSize);
+
     }
 }
