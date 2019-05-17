b073db052d74434df26cf682006f671d760f395b
Zvika Nadav <zvi8875@gmail.com>
2019-05-17 16:44:52 +0300

    set resolution & app icon

--- src/main/java/GUI/MainForm.java
+++ src/main/java/GUI/MainForm.java
@@ -14,8 +14,10 @@ public class MainForm extends JFrame {
     private MenuPanel menuPanel;
     private CardLayout cl;
 
-    public  MainForm(){
-        super("Main form");
+    public MainForm() {
+        super("Restaurant Manager");
+        ImageIcon img = new ImageIcon("src/main/java/GUI/img/icon.png"); //TODO: switch with constant of relative path (src/main/java...)
+        setIconImage(img.getImage());
         cl = new CardLayout();
         containerPanel = new JPanel();
         inventoryPanel = new InventoryPanel();
@@ -24,37 +26,44 @@ public class MainForm extends JFrame {
         OrdersTabs = new JTabbedPane();
         menuPanel = new MenuPanel();
 
-     //////////////// set the layout ////////////////
+        //////////////// set the layout ////////////////
         OrdersTabs.add("Search Orders", ordersSearchPanel);
-        OrdersTabs.add("Add Order",ordersAddPanel);
+        OrdersTabs.add("Add Order", ordersAddPanel);
 
         containerPanel.setLayout(cl);
-        containerPanel.add(inventoryPanel,"Inventory");
-        containerPanel.add(OrdersTabs,"Orders");
-        cl.show(containerPanel,"Inventory");
+        containerPanel.add(inventoryPanel, "Inventory");
+        containerPanel.add(OrdersTabs, "Orders");
+        cl.show(containerPanel, "Inventory");
 
-     ////////////// Menu items listeners ////////////////
+        ////////////// Menu items listeners ////////////////
         menuPanel.inventory.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-               cl.show(containerPanel,"Inventory");
+                cl.show(containerPanel, "Inventory");
             }
         });
         menuPanel.orders.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cl.show(containerPanel,"Orders");
+                cl.show(containerPanel, "Orders");
             }
         });
 
 
         add(menuPanel, BorderLayout.WEST);
-        add(containerPanel,BorderLayout.CENTER);
-
-        setSize(900,700);
+        add(containerPanel, BorderLayout.CENTER);
+        setScreenSize();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        setResizable(false);
 
 
+
+    }
+
+    private void setScreenSize() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width/2, screenSize.height/2);
     }
 
 }
