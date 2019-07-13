54ec35133fe51582ac950b91f6ef87804ac9bc23
Dana <787danam@gmail.com>
2019-07-13 17:02:28 +0300

    Fix class

--- src/main/java/database/RestaurantRepository.java
+++ src/main/java/database/RestaurantRepository.java
@@ -10,29 +10,28 @@ import static main.java.common.constants.DatabaseConstants.*;
 
 public class RestaurantRepository {
 
-        /* Function num #1 - Adding a new restaurant */
+    /* Function num #1 - Adding a new restaurant */
 
-        public static void addRestaurant(Restaurant restaurant) {
-            String name = restaurant.getName();
-            int id = restaurant.getId();
-            String sql = String.format("INSERT INTO restaurant(id,name) VALUES(%s,%s)", RESTAURANT_TABLE_ID_COLUMN, RESTAURANT_TABLE_NAME_COLUMN);
+    public static void addRestaurant(Restaurant restaurant) {
+        String name = restaurant.getName();
+        int id = restaurant.getId();
+        String sql = String.format("INSERT INTO restaurant(id,name) VALUES(%s,%s)", RESTAURANT_TABLE_ID_COLUMN, RESTAURANT_TABLE_NAME_COLUMN);
 
-            Connection conn = DatabaseAccessManager.getConnection();
-            try {
-                PreparedStatement pstmt = conn.prepareStatement(sql);
-                pstmt.setInt(1, id);
-                pstmt.setString(2, name);
-                pstmt.executeUpdate();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                DatabaseAccessManager.closeConnection(conn);
-            }
+        Connection conn = DatabaseAccessManager.getConnection();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DatabaseAccessManager.closeConnection(conn);
         }
-
-
-
     }
 
 
 }
+
+
+
