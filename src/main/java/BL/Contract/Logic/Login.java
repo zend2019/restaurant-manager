92a725f78258c5078b811eba4fe83cb9a08820a2
Noga Aviram <noga.aviram@gmail.com>
2019-07-13 16:35:03 +0300

    Controller logic to Database classes.

--- src/main/java/BL/Contract/Logic/Login.java
+++ src/main/java/BL/Contract/Logic/Login.java
@@ -1,11 +1,11 @@
 package main.java.BL.Contract.Logic;
 
-import main.java.database.DatabaseController;
+import main.java.database.UserRepository;
 
 public class Login implements ILogin {
     @Override
     public boolean LogInSystem(String userName, String password) {
         password = Integer.toString(password.hashCode());
-        return DatabaseController.LogIn(userName, password);
+        return UserRepository.LogIn(userName, password);
     }
 }
