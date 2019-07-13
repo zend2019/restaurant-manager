e5b213239d00365f8246a8797bc7cf2059e918c1
Noga Aviram <noga.aviram@gmail.com>
2019-07-13 18:54:45 +0300

    Order test - add function

---
+++ src/main/java/Test/OrderControllerTest.java
@@ -0,0 +1,38 @@
+import main.java.BL.Contract.Order;
+import main.java.BL.Contract.OrderStatus;
+import main.java.common.exceptions.RestaurantManagerException;
+import main.java.dataAccess.IRestaurantRepository;
+import main.java.database.OrderRepository;
+import org.junit.*;
+
+public class OrderControllerTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public int AddOrder(Order order) {
+        double currentBudget = 120;
+        //TotalAmount = 300
+        if (currentBudget < 300) {
+            throw new RestaurantManagerException("Your order is above the current budget");
+        }
+        int orderId = OrderRepository.addOrder(order);
+
+        return orderId;
+    }
+
+}
--- src/main/java/Test/test.java
+++ src/main/java/Test/test.java
@@ -1,12 +1,9 @@
-package main.java.Test;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class test {
-
     @Test
-    public void testSomething() {
-
+    public void firstTest() {
+        Assert.assertTrue(true);
     }
 }
-
